8ca11b7f5d522ef334c5f55b89f995c3a75b36fc
NoaJin112 <91875044+NoaJin112@users.noreply.github.com>
2023-05-06 14:26:57 +0200

    Scans all hrefs
    Downloads all the hrefs that ends with .xlsx -> long version.

---
+++ iv2.py
@@ -0,0 +1,68 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+from urllib.parse import urljoin
+import urllib.request as urls
+import ssl
+import sys
+from urllib.error import HTTPError as invalid_url
+
+# This is to bypass the SSL certificate error
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+def search(url):
+    list_with_urls = []
+    while True:
+        time.sleep(1)
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        for link in soup.find_all("a"):
+            start_href = link.get("href")
+            if start_href.endswith(".xlsx") or start_href.endswith(".xls"):
+                print(f"Sussy links: {start_href}\n")
+
+
+        for link in soup.find_all("a"):
+            href = link.get("href")
+            full_url = urljoin(url, href)
+            time.sleep(1)
+            response_one = requests.get(full_url)
+            soup_one = BeautifulSoup(response_one.text, "html.parser")
+
+            for link_one in soup_one.find_all("a"):
+                href_one = link_one.get("href")
+                full_url = urljoin(url, href_one)
+                print(f"Pre-URL -> Indirect link to the xlsx file: {full_url}\n")
+                time.sleep(1)
+                response_two = requests.get(full_url)
+                soup_two = BeautifulSoup(response_two.text, "html.parser")
+                    
+                for link_two in soup_two.find_all("a"):
+                    href_two = link_two.get("href")
+                    try:
+                        if href_two.endswith(".xlsx"):
+                            xlsx_file_name = link_two["href"]
+                            final_url = urljoin(url, xlsx_file_name)
+                            print(f"The final download URL: {final_url}\n")
+                            list_with_urls.append(final_url)
+                    except AttributeError:#href_two -> none -> empty
+                        print('AttributeError, no more hrefs to be found, continuing to look for more hrefs......')
+                    except invalid_url:
+                        print('Invalid URL, continuing to look for more hrefs......')
+    
+        try:
+            for download_url in list_with_urls:
+                filename = download_url.split('/')[-1]
+                urls.urlretrieve(download_url, filename)
+                print(f'{filename} has been downloaded from {download_url}')
+        except AttributeError:#href_two -> none -> empty
+            print('AttributeError, no more hrefs to be found.')
+            sys.exit()
+        
+
+
+                    
+                    
+
+
